2974894b4ce6907a01f990e7459983a9f76d4fb7
Mohammad Milad Tufan <m.tufan@student.tugraz.at>
2025-07-31 21:06:53 +0200

    page recognition works, inital lazy Loading implemented

--- frontend/src/app/components/pdf-viewer-component/pdf-viewer-component.ts
+++ frontend/src/app/components/pdf-viewer-component/pdf-viewer-component.ts
@@ -21,6 +21,10 @@ export class PdfViewerComponent {
 	private devicePixelRatio = window.devicePixelRatio || 1;
 	private scale = 1.0 * this.devicePixelRatio;
 	private pageNumberSub!: Subscription;
+	private visiblePages = new Set<number>();
+	private renderedPages = new Set<number>();
+	private alreadyRanObserver = false;
+
 	//=================================================== Public variables ==================================================
 	public totalPages: number = 0;
 	public pdfSrc: string | Uint8Array = ""
@@ -35,23 +39,61 @@ export class PdfViewerComponent {
 	//==================================================== Inputs ===========================================================
 
 	//==================================================== NG ===============================================================
-	ngOnInit() {
-		this.loadPDF();
+	async ngOnInit() {
+		await this.loadPDF();
 		if (this.pdfContainer) {
 			this.pdfViewerService.setPDFScrollContainer(this.pdfContainer);
 			this.pdfContainer.nativeElement.addEventListener('scroll', (event) => {
 				this.pdfViewerService.currentScrollTop = this.pdfContainer.nativeElement.scrollTop;
-				if (this.pdfViewerService.currentScrollTop > (this.pdfViewerService.pageHeight * this.currentPageNumber))
-				{
-					this.currentPageNumber++;
-					this.pdfViewerService.setCurrentPage(this.currentPageNumber)
-				}
+				this.currentPageNumber = this.pdfViewerService.getPageNumberFromScrolltop()
+				this.pdfViewerService.setCurrentPage(this.currentPageNumber);
+				if (!this.alreadyRanObserver) this.createObserver();
 			})
 		}
 
 		this.pageNumberSub = this.pdfViewerService.currentPage$.subscribe(val => {
 			this.currentPageNumber = val;
 		});
+
+	}
+
+	getPageContainer(pageNumber: number) {
+		const container = this.pdfContainer.nativeElement.querySelector(`#pageContainer-${pageNumber}`);
+		return container;
+	}
+
+	async createObserver() {
+		console.log("called Observer")
+		this.alreadyRanObserver = true;
+		const observer = new IntersectionObserver(
+			(entries) => {
+				for (const entry of entries) {
+					const id = entry.target.id;
+					const pageNumber = parseInt(id?.split('-')[1]);
+
+					if (entry.isIntersecting) {
+						this.visiblePages.add(pageNumber);
+						this.rerenderPageOnZoom(pageNumber)
+						observer.unobserve(entry.target);
+					} else {
+						this.visiblePages.delete(pageNumber);
+					}
+				}
+
+				console.log('Visible pages:', Array.from(this.visiblePages));
+			},
+			{
+				root: this.pdfContainer.nativeElement,
+				threshold: 0.01 // Trigger if at least 10% is visible
+			}
+		);
+
+		let pages: any[] = []
+		for(let i = 1; i <= this.totalPages; i++)
+		{
+			pages.push(this.getPageContainer(i))
+		}
+		pages.forEach((page: Element) => observer.observe(page));
 	}
 
 	ngOnDestroy() {
@@ -81,17 +123,44 @@ export class PdfViewerComponent {
 
 				if (this.renderMode == 0) {
 					for (let pageNum = 1; pageNum <= this.totalPages; pageNum++) {
-						this.renderPage(pageNum);
+						this.renderPage(pageNum, true);
 					}
 				}
+				
 			}
 		} catch (error) {
 			console.error("Error loading PDF:", error);
 		}
 	}
 
-	async rerenderPageOnZoom(pageNumber: number)
-	{
+	async unrenderPage(pageNumber: number) {
+		const page = await this.pdfDocument.getPage(pageNumber);
+		const viewport = page.getViewport({ scale: this.scale });
+
+		const container = this.pdfContainer.nativeElement;
+		const canvas = document.createElement("canvas");
+		const text_layer = document.createElement("div");
+		const pageContainer = document.createElement("div");
+		pageContainer.className = "mt-4 mx-auto relative block w-fit h-[900px]";
+		pageContainer.id = `pageContainer-${pageNumber}`;
+		text_layer.className = "text-layer"
+
+		canvas.className = `page-${pageNumber} border border-gray-300 shadow-lg mt-4 mx-auto  h-[900px] w-[600px]`;
+		canvas.id = `page-${pageNumber}`;
+		canvas.appendChild(text_layer);
+
+		pageContainer.appendChild(text_layer)
+		pageContainer.appendChild(canvas)
+
+		const oldPage = container.querySelector("#" + pageContainer.id)
+		const correctCanvas = oldPage?.querySelector("#" + canvas.id)
+
+		if (oldPage && oldPage.parentNode && correctCanvas) {
+			oldPage.parentNode.replaceChild(pageContainer, oldPage)
+		}
+	}
+
+	async rerenderPageOnZoom(pageNumber: number) {
 		const page = await this.pdfDocument.getPage(pageNumber);
 		const viewport = page.getViewport({ scale: this.scale });
 
@@ -110,12 +179,11 @@ export class PdfViewerComponent {
 		pageContainer.appendChild(text_layer)
 		pageContainer.appendChild(canvas)
 
-		
-		const oldPage = container.querySelector("#"+pageContainer.id)
-		const correctCanvas = oldPage?.querySelector("#"+canvas.id)
 
-		if (oldPage && oldPage.parentNode && correctCanvas)
-		{
+		const oldPage = container.querySelector("#" + pageContainer.id)
+		const correctCanvas = oldPage?.querySelector("#" + canvas.id)
+
+		if (oldPage && oldPage.parentNode && correctCanvas) {
 			oldPage.parentNode.replaceChild(pageContainer, oldPage)
 		}
 
@@ -132,23 +200,45 @@ export class PdfViewerComponent {
 		this.pdfViewerService.setPageHeight(viewport.height);
 
 		await page.render(renderContext).promise;
-		
+		this.renderedPages.add(pageNumber)
 	}
 
 	// Render a specific page of the PDF.
-	async renderPage(pageNumber: number) {
-		const page = await this.pdfDocument.getPage(pageNumber);
-		const viewport = page.getViewport({ scale: this.scale });
+	async renderPage(pageNumber: number, renderdummy: Boolean = true) {
+		let page: any;
+		let viewport: any;
+		if (pageNumber === 1)
+			renderdummy = false;
+
+		page = await this.pdfDocument.getPage(pageNumber);
+
+		if (!renderdummy)
+		{
+			page = await this.pdfDocument.getPage(pageNumber);
+			viewport = page.getViewport({ scale: this.scale });
+			this.pdfViewerService.setPageHeight(viewport.height);
+		}
 
 		const container = this.pdfContainer.nativeElement;
 		const canvas = document.createElement("canvas");
 		const text_layer = document.createElement("div");
 		const pageContainer = document.createElement("div");
-		pageContainer.className = "mt-4 mx-auto relative block w-fit";
+		if (!renderdummy)
+		{
+			pageContainer.className = "mt-4 mx-auto relative block w-fit";
+			canvas.className = `page-${pageNumber} border border-gray-300 shadow-lg mt-4 mx-auto`;
+
+		}
+		else
+		{
+			pageContainer.className = `mt-4 mx-auto relative block w-fit h-[900px]`;
+			canvas.className = `page-${pageNumber} border border-gray-300 shadow-lg mt-4  h-[900px] w-[600px]`;
+		}
+			
 		pageContainer.id = `pageContainer-${pageNumber}`;
 		text_layer.className = "text-layer"
+
 		
-		canvas.className = `page-${pageNumber} border border-gray-300 shadow-lg mt-4 mx-auto`;
 		canvas.id = `page-${pageNumber}`;
 		canvas.appendChild(text_layer);
 
@@ -157,33 +247,32 @@ export class PdfViewerComponent {
 
 		container.appendChild(pageContainer);
 
-		const context = canvas.getContext("2d")!;
-		canvas.height = viewport.height;
-		canvas.width = viewport.width;
+		if (!renderdummy)
+		{
+			const context = canvas.getContext("2d")!;
+			canvas.height = viewport.height;
+			canvas.width = viewport.width;
 
-		const renderContext = {
-			canvasContext: context,
-			viewport: viewport,
-			intent: "print"
-		};
+			const renderContext = {
+				canvasContext: context,
+				viewport: viewport,
+				intent: "print"
+			};
 
-		this.pdfViewerService.setPageHeight(viewport.height);
+			await page.render(renderContext).promise;
+		}
 
-		await page.render(renderContext).promise;
 	}
 
-	public onWheel(event: WheelEvent)
-	{
+	public onWheel(event: WheelEvent) {
 		if (event.ctrlKey) {
 			event.preventDefault();
-			if (event.deltaY < 0)
-			{
-				this.scale += 0.25;
+			if (event.deltaY < 0) {
+				this.scale += 0.1;
 				this.rerenderPageOnZoom(this.currentPageNumber)
 			}
-			else
-			{
-				this.scale -= 0.25;
+			else {
+				this.scale -= 0.1;
 				this.rerenderPageOnZoom(this.currentPageNumber)
 			}
 		}
--- frontend/src/app/services/pdfviewer-service.ts
+++ frontend/src/app/services/pdfviewer-service.ts
@@ -29,10 +29,33 @@ export class PDFViewerService {
 		this.PDFScrollContainer = scrollContainer;
 	}
 
+	getPageNumberFromScrolltop(scrolltop: number | null = null)
+	{
+		let pageNum: number  = this.totalPages;
+		if (scrolltop === null) scrolltop = this.currentScrollTop;
+
+		for(let i = 1; i < this.totalPages; i++)
+		{	// e.g. check if it between scrollTop of page 1 and scrolltop of page 2 ==> current page is 1
+			const firsCmp = ((i-1) * (this.pageHeight))
+			const scndCmp = ((i) * (this.pageHeight))
+			if (scrolltop > firsCmp && scrolltop < scndCmp)
+			{
+				pageNum = i;
+				break;
+			}
+		}
+		return pageNum;
+	}
+
+	calcTargetScrolltop(pageNumber: number)
+	{
+		return (pageNumber - 1) * (this.pageHeight + 17);
+	}
+
 	scrollToPage(pageNumber: number)
 	{
 		console.log("Jump to page: ", pageNumber);
-		const targetScrolltop = (pageNumber - 1) * (this.pageHeight + 17);
+		const targetScrolltop = this.calcTargetScrolltop(pageNumber);
 		this.PDFScrollContainer!.nativeElement.scrollTop = targetScrolltop;
 		// this.PDFScrollContainer!.nativeElement.scroll({top: targetScrolltop, bevahior: 'smooth'})
 	}
