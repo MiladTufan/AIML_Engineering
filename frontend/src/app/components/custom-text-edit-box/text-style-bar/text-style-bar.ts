e67e69045f110f2cfd5e0eebb917a9d6f5157fa3
Mohammad Milad Tufan <m.tufan@student.tugraz.at>
2025-08-07 17:19:53 +0200

    added font dropDown

--- frontend/src/app/components/custom-text-edit-box/text-style-bar/text-style-bar.ts
+++ frontend/src/app/components/custom-text-edit-box/text-style-bar/text-style-bar.ts
@@ -14,12 +14,21 @@ export class TextStyleBar {
 	constructor(public textEditService: TextEditService) { }
 
 	isCollapsed: Boolean = true;
+	isFontDropDownOpen: Boolean = false;
 
 	colors: string[] = [
 		'#000000', '#8B0000', '#800000', '#8B4513', '#FF8C00',
 		'#DAA520', '#808000', '#228B22', '#008080', '#4682B4',
 		'#4169E1', '#4B0082', '#9400D3', '#BA55D3', '#DDA0DD',
 		'#708090', '#778899', '#D3D3D3', '#DCDCDC', '#FFFFFF'
+	];
+
+	fontOptions = [
+		{ name: 'Roboto', value: 'Roboto, sans-serif' },
+		{ name: 'Georgia', value: 'Georgia, serif' },
+		{ name: 'Courier New', value: '"Courier New", monospace' },
+		{ name: 'Comic Sans', value: '"Comic Sans MS", cursive, sans-serif' },
+		{ name: 'Arial', value: 'Arial, sans-serif' }
 	];
 
 	onColorSelect(color: string) {
@@ -32,6 +41,11 @@ export class TextStyleBar {
 
 	collapseColorPallet() {
 		this.textEditService.getCurrentTextStyleEditor().isCollapsed = true;
+	}
+
+
+	toggleDowpDown() {
+		this.isFontDropDownOpen = !this.isFontDropDownOpen;
 	}
 
 }
